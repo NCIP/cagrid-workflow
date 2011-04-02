07cf3d010e8bed9b35b13a05ea574c49b5113eaf
mkedzireski <mkedzireski>
2011-04-02 20:50:42 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cagrid/trunk/cagrid/Software/workflow@17568 dd7b0c16-3c94-4492-92de-414b6a06f0b1

---
+++ workflow-portlet/src/main/java/gov/nih/nci/cagrid/portal/portlet/workflow/util/Utils.java
@@ -0,0 +1,65 @@
+package gov.nih.nci.cagrid.portal.portlet.workflow.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Utils
+ * TODO type-comment Utils
+ * @author Marek Kedzierski (kedzie@ci.uchicago.edu)
+ * @created Apr 2, 2011 - 2011
+ */
+public class Utils implements ApplicationContextAware {
+	private static final Log log = LogFactory.getLog(Utils.class);
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	/**
+	 * Download a file
+	 * @param uri address of file to download
+	 * @return file contents
+	 * @throws IOException 
+	 */
+	public byte[] download(String uri) throws IOException {
+		log.debug("Downloading File: " + uri);
+		InputStream is = null;
+		try {
+			is = getApplicationContext().getResource(uri).getInputStream();
+			byte[] bytes = new byte[is.available()];
+			is.read(bytes);
+			return bytes;
+		} finally { if(is!=null) is.close(); }
+	}
+	
+	/**
+	 * Write binary data to filesystem; creating all necessary subfolders.
+	 * @param filename the filename
+	 * @param contents file contents
+	 */
+	public void saveFile(String filename, byte[] contents) throws IOException {
+		FileOutputStream fos = null;
+		try {
+			new File(filename).mkdirs();
+			fos = new FileOutputStream(filename);
+			fos.write(contents);
+			log.debug("Wrote" + contents.length + " bytes to: " + filename);
+		} catch(FileNotFoundException e) {} finally { if(fos!=null) fos.close(); }
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+}
