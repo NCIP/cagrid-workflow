38292b0ff9671081824fdcbd6724871032616656
justink140 <justin.kuss@inventrio.com>
2013-05-29 15:13:59 -0400

    Updating license headers for source code

--- cagrid-activity-ui/src/main/java/net/sf/taverna/t2/activities/cagrid/views/CaGridActivityViewFactory.java
+++ cagrid-activity-ui/src/main/java/net/sf/taverna/t2/activities/cagrid/views/CaGridActivityViewFactory.java
@@ -1,3 +1,13 @@
+/**
+*============================================================================
+*  The Ohio State University Research Foundation, The University of Chicago -
+*  Argonne National Laboratory, Emory University, SemanticBits LLC, 
+*  and Ekagra Software Technologies Ltd.
+*
+*  Distributed under the OSI-approved BSD 3-Clause License.
+*  See http://ncip.github.com/cagrid-core/LICENSE.txt for details.
+*============================================================================
+**/
 package net.sf.taverna.t2.activities.cagrid.views;
 
 import net.sf.taverna.t2.activities.wsdl.WSDLActivity;
--- wsdl-generic/src/main/java/net/sf/taverna/wsdl/xmlsplitter/XMLInputSplitter.java
+++ wsdl-generic/src/main/java/net/sf/taverna/wsdl/xmlsplitter/XMLInputSplitter.java
@@ -1,3 +1,13 @@
+/**
+*============================================================================
+*  The Ohio State University Research Foundation, The University of Chicago -
+*  Argonne National Laboratory, Emory University, SemanticBits LLC, 
+*  and Ekagra Software Technologies Ltd.
+*
+*  Distributed under the OSI-approved BSD 3-Clause License.
+*  See http://ncip.github.com/cagrid-core/LICENSE.txt for details.
+*============================================================================
+**/
 package net.sf.taverna.wsdl.xmlsplitter;
 
 import java.io.IOException;
